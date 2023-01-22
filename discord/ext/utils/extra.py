3d4cabbbee9f4561bf7a24c51dce85e42e4cc5b6
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2023-01-21 18:25:10 -0800

    added license

--- discord/ext/utils/extra.py
+++ discord/ext/utils/extra.py
@@ -1,4 +1,30 @@
+"""
+MIT License
+
+Copyright (c) 2023-present cibere
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 from typing import Literal, Optional, Sequence, Union, overload
+
+from discord.ui import View
+from discord.utils import MISSING
 
 import discord
 from discord import (
@@ -12,8 +38,6 @@ from discord import (
     StickerItem,
 )
 from discord.ext import commands
-from discord.ui import View
-from discord.utils import MISSING
 
 
 def _pop(item: dict, items: list):
